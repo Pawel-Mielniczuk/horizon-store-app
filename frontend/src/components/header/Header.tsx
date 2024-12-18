4ba05fe26eb286ca6f54acd2709d3d6752cc7eba
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-18 13:23:17 +0100

    create logo component

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { Nav } from "../nav/Nav";
 import { HeaderStyles } from "./HeaderStyles";
+import { Logo } from "./Logo";
 
 export function Header() {
   return (
     <HeaderStyles>
       <div className="bar">
-        <Link to="/">HorisonGear</Link>
+        <Logo>
+          <Link to="/">HorisonGear</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <input type="text" />
---
+++ frontend/src/components/header/Logo.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Logo = styled.h1`
+  background-color: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
