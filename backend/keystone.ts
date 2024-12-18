5554f6bf6697dd3296830b2a6484034115353549
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-18 14:25:47 +0100

    add server and cors configuration

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,27 +1,24 @@
-// Welcome to Keystone!
-//
-// This file is what Keystone uses as the entry-point to your headless backend
-//
-// Keystone imports the default export of this file, expecting a Keystone configuration object
-//   you can find out more at https://keystonejs.com/docs/apis/config
-
+import "dotenv/config";
 import { config } from "@keystone-6/core";
 
-// to keep this file tidy, we define our schema in a different file
 import { lists } from "./schema";
 
-// authentication is configured separately here too, but you might move this elsewhere
-// when you write your list-level access control functions, as they typically rely on session data
 import { withAuth, session } from "./auth";
 
 export default withAuth(
   config({
     db: {
-      // we're using sqlite for the fastest startup experience
-      //   for more information on what database might be appropriate for you
-      //   see https://keystonejs.com/docs/guides/choosing-a-database#title
       provider: "sqlite",
       url: "file:./keystone.db",
+    },
+    server: {
+      cors: {
+        csrfPrevention: false,
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization", "x-apollo-operation-name", "apollo-require-preflight"],
+        methods: ["GET", "POST", "OPTIONS"],
+      },
     },
     lists,
     session,
