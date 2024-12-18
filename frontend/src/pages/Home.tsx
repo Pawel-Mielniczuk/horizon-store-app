3c77460dfdb08d45b36567099c124b6a5c5db643
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-18 13:35:25 +0100

    create routes in router

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,10 @@
 import { Layout } from "../components/layout/Layout.tsx";
+import { Outlet } from "react-router-dom";
 
 export function Home() {
-  return <Layout />;
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  );
 }
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,14 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import { SellPage } from "../pages/SellPage";
+import { OrdersPage } from "../pages/OrdersPage.tsx";
 import { Home } from "../pages/Home.tsx";
+import { AccountPage } from "../pages/AccountPage.tsx";
+import { ProductsPage } from "../pages/ProductsPage.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
-  {
-    path: "/sell",
-    element: <SellPage />,
+    children: [
+      {
+        path: "/products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/sell",
+        element: <SellPage />,
+      },
+      {
+        path: "/orders",
+        element: <OrdersPage />,
+      },
+      {
+        path: "/account",
+        element: <AccountPage />,
+      },
+    ],
   },
 ]);
