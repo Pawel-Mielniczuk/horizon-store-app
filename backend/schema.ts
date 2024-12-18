6e3682a18c58939db0ad5bbdf380153c7acecd82
Pawel Mielniczuk <mrjeronimo@onet.pl>
2024-12-18 14:38:37 +0100

    create Product data type

--- backend/schema.ts
+++ backend/schema.ts
@@ -1,6 +1,6 @@
 import { list } from "@keystone-6/core";
 import { allowAll } from "@keystone-6/core/access";
-import { text, relationship, password, timestamp, select } from "@keystone-6/core/fields";
+import { text, relationship, password, timestamp, select, integer } from "@keystone-6/core/fields";
 import { document } from "@keystone-6/fields-document";
 
 import { type Lists } from ".keystone/types";
@@ -15,66 +15,35 @@ export const lists = {
         isIndexed: "unique",
       }),
       password: password({ validation: { isRequired: true } }),
-      posts: relationship({ ref: "Post.author", many: true }),
       createdAt: timestamp({
         defaultValue: { kind: "now" },
       }),
     },
   }),
-
-  Post: list({
+  Product: list({
     access: allowAll,
     fields: {
-      title: text({ validation: { isRequired: true } }),
-      content: document({
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1],
+      name: text({ validation: { isRequired: true } }),
+      description: text({
+        ui: {
+          displayMode: "textarea",
+        },
+      }),
+      status: select({
+        options: [
+          { label: "Draft", value: "DRAFT" },
+          { label: "Available", value: "AVAILABLE" },
+          { label: "Unavailable", value: "UNAVAILABLE" },
         ],
-        links: true,
-        dividers: true,
-      }),
-
-      author: relationship({
-        ref: "User.posts",
-
+        defaultValue: "DRAFT",
         ui: {
-          displayMode: "cards",
-          cardFields: ["name", "email"],
-          inlineEdit: { fields: ["name", "email"] },
-          linkToItem: true,
-          inlineConnect: true,
-        },
-
-        many: false,
-      }),
-      tags: relationship({
-        ref: "Tag.posts",
-        many: true,
-        ui: {
-          displayMode: "cards",
-          cardFields: ["name"],
-          inlineEdit: { fields: ["name"] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ["name"] },
+          displayMode: "segmented-control",
+          createView: {
+            fieldMode: "hidden",
+          },
         },
       }),
-    },
-  }),
-  Tag: list({
-    access: allowAll,
-    ui: {
-      isHidden: true,
-    },
-    fields: {
-      name: text(),
-
-      posts: relationship({ ref: "Post.tags", many: true }),
+      price: integer(),
     },
   }),
 } satisfies Lists;
